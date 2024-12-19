66341e0bc2a313d2ed74ce8f97a9d21d36f201a6
KristoMatsumoto <118841278+KristoMatsumoto@users.noreply.github.com>
2024-12-19 16:49:21 +0100

    Rules model

---
+++ app/models/rules.rb
@@ -0,0 +1,21 @@
+class Rules
+  Rule = Struct.new(:id, :title, :description, :icon, :option)
+  @@rules = []
+
+  def self.load_rules
+    @@rules = [
+      Rule.new('skip_after_draw_card_from_dropping', "Draw only 1", "Player can take only 1 card from the desk per turn", 
+        '', 'check_box'),
+      Rule.new('skip_after_draw_cards', "Draw and skip turn", "After player takes cards after +2 and/or +4, he misses his turn", 
+        '', 'check_box'),
+    ]
+  end
+
+  def self.all
+    @@rules
+  end
+
+  def self.find(id)
+    @@rules.find { |rule| rule.id == id }
+  end
+end
